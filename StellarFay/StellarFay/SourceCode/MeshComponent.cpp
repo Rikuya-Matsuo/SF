4c743eac0976007ee4a55d9f2f68368a896d786c
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2021-04-14 18:34:05 +0900

    MeshComponentをちょっと作成

---
+++ StellarFay/StellarFay/SourceCode/MeshComponent.cpp
@@ -0,0 +1,14 @@
+﻿#include "MeshComponent.h"
+#include "Actor.h"
+#include "Mesh.h"
+
+MeshComponent::MeshComponent(Actor * owner, Mesh * msh, int drawPriority) :
+	ComponentBase(owner, 1000),
+	mMesh(msh),
+	mDrawPriority(drawPriority)
+{
+}
+
+MeshComponent::~MeshComponent()
+{
+}
---
+++ StellarFay/StellarFay/SourceCode/MeshComponent.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "ComponentBase.h"
+
+class MeshComponent : public ComponentBase
+{
+public:
+	MeshComponent(class Actor * owner, class Mesh * msh, int drawPriority);
+	~MeshComponent();
+
+private:
+	// メッシュ
+	class Mesh * mMesh;
+
+	// 描画順
+	// 半透明な部分に関してはこの値に関係なく、カメラからの距離で描画順を決定する予定
+	int mDrawPriority;
+};
