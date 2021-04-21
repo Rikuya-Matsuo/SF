c0721cf77e15ccc0f46c4ca4d00b7e645232b85d
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2021-04-21 11:22:43 +0900

    VertexArrayをクラス化中。今回使うメッシュシステムの仕様を鑑みて、インデックスバッファはこれに備えないことにした。

---
+++ StellarFay/StellarFay/SourceCode/VertexArray.cpp
@@ -0,0 +1,62 @@
+﻿#include "VertexArray.h"
+
+#
+
+VertexArray::VertexArray(const void * verts, size_t vertsMass, size_t * attributeSizes, size_t attributeSizeElemMass, GLenum type, GLboolean normalize, GLenum usage)
+{
+	// 頂点配列、頂点バッファ生成
+	glGenVertexArrays(1, &mVAO);
+	glGenBuffers(1, &mVBO);
+
+	// 頂点配列バインド
+	glBindVertexArray(mVAO);
+
+	// 頂点の配列の型のサイズを計算
+	// unsignedがついてもサイズは変わらないため、処理を同じにしている
+	size_t vertTypeSize;
+	switch (type)
+	{
+	case GL_UNSIGNED_INT:
+	case GL_INT:
+		vertTypeSize = sizeof(int);
+		break;
+
+	case GL_UNSIGNED_SHORT:
+	case GL_SHORT:
+		vertTypeSize = sizeof(short);
+		break;
+
+	case GL_UNSIGNED_BYTE:
+	case GL_BYTE:
+		vertTypeSize = sizeof(char);
+		break;
+
+	case GL_FLOAT:
+		vertTypeSize = sizeof(float);
+		break;
+
+	default:
+		break;
+	}
+
+	// 頂点バッファに頂点を設定
+	glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+	glBufferData(GL_ARRAY_BUFFER, vertTypeSize * vertsMass, verts, usage);
+
+	// 頂点データのストライド設定のため、attributeSizesの合計値を計算
+	size_t attSizesSum = 0;
+	for (size_t i = 0; i < attributeSizeElemMass; ++i)
+	{
+		attSizesSum += attributeSizes[i];
+	}
+
+	// 頂点データの解釈方法設定
+	const size_t stride = vertTypeSize * attSizesSum;
+	size_t attSizesSumBuf = 0;
+	for (size_t i = 0; i < attributeSizeElemMass; ++i)
+	{
+		glVertexAttribPointer(i, attributeSizes[i], type, normalize, stride, (void*)(vertTypeSize * attSizesSumBuf));
+		glEnableVertexAttribArray(i);
+		attSizesSumBuf += attributeSizes[i];
+	}
+}
---
+++ StellarFay/StellarFay/SourceCode/VertexArray.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <glad/glad.h>
+#include <vector>
+
+class VertexArray
+{
+public:
+	// verts					: 頂点データ
+	// vertsMass				: 頂点データの要素数
+	// attributeSize			: 頂点データ解釈方法（glVertexAttribPointerの第二引数を順番に、配列の形で入力すべし）
+	// attributeSizeElemMass	: attributeSizeの要素数
+	VertexArray(const void * verts, size_t vertsMass, size_t * attributeSizes, size_t attributeSizeElemMass, GLenum type, GLboolean normalize, GLenum usage = GL_STATIC_DRAW);
+
+private:
+	GLuint mVAO;
+	GLuint mVBO;
+
+	static std::vector<size_t> Attribute;
+};
