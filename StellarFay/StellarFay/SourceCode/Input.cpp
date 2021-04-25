b97affd9776c78998a21f46cf5000613f04486aa
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2021-04-25 15:54:59 +0900

    Inputクラスを作成。キーボード操作であれば恐らく不都合はないところまでは来た。

---
+++ StellarFay/StellarFay/SourceCode/Input.cpp
@@ -0,0 +1,37 @@
+﻿#include "Input.h"
+
+Input::Input() :
+	mQuitEventFlag(false)
+{
+	// キー状態にアクセスできるポインタを取得
+	mKeyStates = SDL_GetKeyboardState(nullptr);
+
+	// 前フレームのキー状態を初期化
+	for (int i = 0; i < SDL_NUM_SCANCODES; ++i)
+	{
+		mPrevKeyStates[i] = 0;
+	}
+}
+
+Input::~Input()
+{
+}
+
+void Input::Update()
+{
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent))
+	{
+		// 終了フラグを立てる
+		if (sdlEvent.type == SDL_QUIT)
+		{
+			mQuitEventFlag = true;
+		}
+	}
+}
+
+void Input::LastUpdate()
+{
+	// キー状態保存
+	SDL_memcpy(mPrevKeyStates, mKeyStates, SDL_NUM_SCANCODES);
+}
---
+++ StellarFay/StellarFay/SourceCode/Input.h
@@ -0,0 +1,46 @@
+﻿#include <SDL/SDL.h>
+
+// 入力を管理するシングルトンクラス
+class Input final
+{
+public:
+	~Input();
+
+	static Input & GetInstance()
+	{
+		static Input instance;
+		return instance;
+	}
+
+	// 現フレームでキーが押されているか
+	bool GetKey(int scanCode) const { return mKeyStates[scanCode]; }
+
+	// このフレームでキーが押されたなら真を返す
+	bool GetKeyPressDown(int scanCode) const { return (mKeyStates[scanCode] && !mPrevKeyStates[scanCode]); }
+
+	// このフレームでキーが離されたなら真を返す
+	bool GetKeyRelease(int scanCode) const { return (!mKeyStates[scanCode] && mPrevKeyStates[scanCode]); }
+
+	// 終了イベントフラグの取得
+	bool GetQuitEventFlag() const { return mQuitEventFlag; }
+
+	// 更新処理
+	void Update();
+
+	// フレームの最後に呼ぶ。主に入力状態の記録が目的
+	void LastUpdate();
+
+private:
+	Input();
+
+	// キー状態
+	const Uint8 * mKeyStates;
+
+	// 前フレームのキー状態
+	Uint8 mPrevKeyStates[SDL_NUM_SCANCODES];
+
+	// 終了イベントが発生したフラグ
+	bool mQuitEventFlag;
+};
+
+#define INPUT_INSTANCE Input::GetInstance()
