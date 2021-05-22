770aaf1b60fdc90aa6ae69a67f89b2fb7ef57a3c
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2021-05-22 10:37:47 +0900

    UI用シェーダのラッパーを作成。

---
+++ StellarFay/StellarFay/SourceCode/SpriteShaderWrapper.cpp
@@ -0,0 +1,21 @@
+﻿#include "SpriteShaderWrapper.h"
+#include "GameSystem.h"
+#include "Renderer.h"
+
+Shader * SpriteShaderWrapper::mSpriteShader = nullptr;
+
+SpriteShaderWrapper::SpriteShaderWrapper(Shader * shader) :
+	ShaderWrapper()
+{
+	if (!mSpriteShader)
+	{
+		mSpriteShader = RENDERER_INSTANCE.GetShader("Shaders/Sprite.vert", "Shaders/Sprite.frag");
+	}
+
+	mShader = (!shader) ? mSpriteShader : shader;
+}
+
+//////////////////////////////////////////////////////////////
+// Project		: StellarFay
+// Copyright (c) 2021 Rikuya Matsuo. All rights reserved.
+//////////////////////////////////////////////////////////////
---
+++ StellarFay/StellarFay/SourceCode/SpriteShaderWrapper.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "ShaderWrapper.h"
+
+class SpriteShaderWrapper : public ShaderWrapper
+{
+public:
+	SpriteShaderWrapper(Shader * shader = nullptr);
+
+private:
+	static Shader * mSpriteShader;
+};
+
+//////////////////////////////////////////////////////////////
+// Project		: StellarFay
+// Copyright (c) 2021 Rikuya Matsuo. All rights reserved.
+//////////////////////////////////////////////////////////////
