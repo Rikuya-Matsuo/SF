14d4a52f5614a253dfa22b2da84704fbcf1b68a2
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2021-05-24 10:51:16 +0900

    UIのマネージャクラスを作成。予定として、ゲームシステムにて生成し、そのポインタをレンダラーと共有する仕組みを想定している。

--- StellarFay/StellarFay/SourceCode/UILayerBase.cpp
+++ StellarFay/StellarFay/SourceCode/UILayerBase.cpp
@@ -1,5 +1,31 @@
 ﻿#include "UILayerBase.h"
 
-UILayerBase::UILayerBase()
+UILayerBase::UILayerBase(int drawOrder, int updateOrder) :
+	mDrawPriority(drawOrder),
+	mDrawFlag(true)
 {
 }
+
+void UILayerBase::Update()
+{
+}
+
+void UILayerBase::Draw() const
+{
+}
+
+void UILayerBase::UpdateUI()
+{
+}
+
+void UILayerBase::SetDrawPriority(int drawPriority)
+{
+	mDrawFlag = drawPriority;
+
+	// マネージャにソートを要請する
+}
+
+//////////////////////////////////////////////////////////////
+// Project		: StellarFay
+// Copyright (c) 2021 Rikuya Matsuo. All rights reserved.
+//////////////////////////////////////////////////////////////
---
+++ StellarFay/StellarFay/SourceCode/UILayerManager.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <list>
+
+// UIのレイヤーを管理するクラス
+class UILayerManager final
+{
+public:
+	UILayerManager();
+	~UILayerManager();
+
+	// 更新
+	void Update();
+
+	// 描画
+	void Draw();
+
+	// UIの登録
+	void RegisterUILayer(class UILayerBase * ui);
+
+	// UIの登録解除
+	void DeregisterUILayer(class UILayerBase * ui);
+
+	// 描画リストのソート要請
+	void RequestSortDrawList() { mSortDrawListFlag = true; }
+
+	// 更新リストのソート要請
+	void RequestSortUpdateList() { mSortUpdateListFlag = true; }
+
+private:
+	// 描画用リスト
+	std::list<class UILayerBase *> mDrawList;
+
+	// 描画リストソートフラグ
+	bool mSortDrawListFlag;
+
+	// 更新リストソートフラグ
+	bool mSortUpdateListFlag;
+
+	// 描画用リストへの登録
+	void RegisterIntoDrawList(class UILayerBase * ui);
+};
+
+//////////////////////////////////////////////////////////////
+// Project		: StellarFay
+// Copyright (c) 2021 Rikuya Matsuo. All rights reserved.
+//////////////////////////////////////////////////////////////
