9dacdb0360775ea21f198516351d9c021b37a37b
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2021-05-31 18:55:24 +0900

    ゲームパッドのホットスワップを一部実装。

--- StellarFay/StellarFay/SourceCode/Input.cpp
+++ StellarFay/StellarFay/SourceCode/Input.cpp
@@ -38,10 +38,23 @@ void Input::Update()
 	while (SDL_PollEvent(&sdlEvent))
 	{
 		// 終了フラグを立てる
-		if (sdlEvent.type == SDL_QUIT)
+		if (sdlEvent.type == SDL_EventType::SDL_QUIT)
 		{
 			mQuitEventFlag = true;
 		}
+
+		// ゲームパッドが接続される
+		if (sdlEvent.type == SDL_EventType::SDL_CONTROLLERDEVICEADDED)
+		{
+			const int index = sdlEvent.cdevice.which;
+			if (index < mMaxGamePadMass)
+			{
+				mGamePads[index].Connect(index);
+			}
+		}
+
+		// ゲームパッドが外される
+
 	}
 
 	// キーボード入力状態はSDLによって自動更新されている
